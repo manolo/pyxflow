8b8bea2513353d239b2d3408b968c5ad63c8023a
Manolo Carrasco <manolo@vaadin.com>
2026-03-03 16:28:55 +0100

    Bump version to 0.4.0 for first PyPI release

--- src/pyxflow/__init__.py
+++ src/pyxflow/__init__.py
@@ -1,6 +1,6 @@
 """Vaadin Flow for Python - Server-side UI framework."""
 
-__version__ = "0.1.0"
+__version__ = "0.4.0"
 
 from pyxflow.core.component import Component
 from pyxflow.core.element import Element
